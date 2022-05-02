54e039e5ae6e0e8a58a7dd83e1ce3c9f789fc5ae
Temirlan <tbulebaev@bk.ru>
2022-05-02 19:44:28 +0300

    Initial commit

---
+++ portfolio/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)# ограничеваем количество символов
+    description = models.CharField(max_length=250)# значение для короткого описания проекта
+    image = models.ImageField(upload_to='portfolio/images/')# Откуда будут браться изображения
+    url = models.URLField(blank=True)# Позволяет открывать страницу на новой вкладке браузера
